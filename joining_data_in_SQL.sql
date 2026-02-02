455b279e7d781547cee781de7014caca5843f2e8
kellenhosfeld <kellenhosfeld@gmail.com>
2026-02-02 15:41:34 -0500

    Changes to Joining in SQL after review

--- joining_data_in_SQL.sql
+++ joining_data_in_SQL.sql
@@ -1,6 +1,8 @@
 -- How many rows are in the athletes table? How many distinct athlete ids?
 
-SELECT COUNT(id), COUNT(DISTINCT id)
+-- We use COUNT(*) instead of COUNT(id) because COUNT(id) does not count null values
+
+SELECT COUNT(*), COUNT(DISTINCT id)
 FROM athletes;
 
 -- Which years are represented in the summer_games, winter_games, and country_stats tables?
@@ -14,13 +16,14 @@ FROM winter_games;
 SELECT DISTINCT year
 FROM country_stats;
 
-SELECT DISTINCT country_id, sg.year AS summer, wg.year AS winter, cs.year AS country_stats
-FROM summer_games sg
-INNER JOIN winter_games wg
-	USING(country_id)
-INNER JOIN country_stats cs
-	USING(country_id)
-ORDER BY country_id;
+SELECT year
+FROM summer_games
+UNION
+SELECT YEAR
+FROM winter_games
+UNION
+SELECT year::DATE
+FROM country_stats;
 
 -- How many distinct countries are represented in the countries and country_stats table?
 
@@ -46,12 +49,20 @@ ORDER BY athlete_count;
 
 -- Write a query to list countries by total bronze medals, with the highest totals at the top and nulls at the bottom.
 
-SELECT country_id, COUNT(sg.bronze) + COUNT(wg.bronze) AS num_bronze_medals
-FROM summer_games AS sg
-	FULL JOIN winter_games AS wg
-	USING(country_id)
-GROUP BY country_id
-ORDER BY num_bronze_medals DESC;
+-- SELECT country_id, COUNT(sg.bronze) + COUNT(wg.bronze) AS num_bronze_medals
+-- FROM summer_games AS sg
+-- 	FULL JOIN winter_games AS wg
+-- 	USING(country_id)
+-- GROUP BY country_id
+-- ORDER BY num_bronze_medals DESC;
+
+SELECT country, SUM(bronze) AS num_bronze_medals
+FROM countries
+	LEFT JOIN summer_games
+	ON countries.id = summer_games.country_id
+GROUP BY country
+ORDER BY num_bronze_medals DESC NULLS LAST
+LIMIT 1;
 
 -- Adjust the query to only return the country with the most bronze medals
 
@@ -73,22 +84,26 @@ INNER JOIN countries AS c
 ON cs.country_id = id
 INNER JOIN winter_games AS wg
 ON id = wg.country_id
-GROUP BY country;
+GROUP BY country
+ORDER BY avg_pop_in_millions DESC;
 
 -- Identify countries where the population decreased from 2000 to 2006
-SELECT c.country, year, pop_in_millions
-FROM country_stats
-FULL JOIN  countries c
-ON country_id = id
-WHERE year LIKE '2000%' OR year LIKE '2006%';
 
-SELECT * FROM country_stats WHERE year = '2000' OR year = '2006';
+-- SELECT c.country, year, pop_in_millions
+-- FROM country_stats
+-- FULL JOIN  countries c
+-- ON country_id = id
+-- WHERE year LIKE '2000%' OR year LIKE '2006%';
 
-SELECT * FROM country_stats;
+-- SELECT * FROM country_stats WHERE year = '2000' OR year = '2006';
 
-SELECT country_id, cs00.pop_in_millions AS pop_2000, cs00.year, cs06.pop_in_millions AS pop_2006, cs06.year
+-- SELECT * FROM country_stats;
+
+SELECT country, cs00.pop_in_millions AS pop_2000, cs00.year, cs06.pop_in_millions AS pop_2006, cs06.year
 FROM country_stats AS cs00
-	INNER JOIN country_stats AS cs06
-	USING(country_id)
+INNER JOIN country_stats AS cs06
+USING(country_id)
+INNER JOIN countries
+ON country_id = id
 WHERE cs00.year = '2000-01-01' AND cs06.year = '2006-01-01'
 AND CAST(cs00.pop_in_millions AS NUMERIC) > CAST(cs06.pop_in_millions AS NUMERIC);
